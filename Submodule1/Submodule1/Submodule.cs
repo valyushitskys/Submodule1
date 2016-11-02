1d765b59519842e2448a9f9e105a273450bb8de3
valyushitskys <valyushitskys@USTN-DEV06>
2016-11-02 08:12:04 -0500

    Add submodule code.

---
+++ Submodule1/Submodule1/Submodule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Submodule1
+{
+    public class Submodule
+    {
+        public void WriteLine()
+        {
+            Console.WriteLine("Submodule 1");
+        }
+    }
+}
